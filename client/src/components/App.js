a2d0110a48c34fbe2bce106491421aaad0aabad4
Amie Foster <amie.n.foster@gmail.com>
2021-11-26 14:40:28 -0700

    deleting react router

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,6 +3,7 @@ import '../App.css';
 function App() {
   return (
     <div>
+      
         <div class="container">
 
           <div class="row justify-content-center">
---
+++ client/src/components/Homepage.js
@@ -0,0 +1,11 @@
+
+function Homepage() {
+
+    return (
+        <div>
+            <h1>This is the homepage!</h1>
+        </div>
+    )
+}
+
+export default Homepage;
